9099774ebf893853e7246a910341bab7ac183b9e
Evhen Malysh <malysh.evgeniy@gmail.com>
2025-09-11 21:27:29 +0300

    feat(crypto): add tests for crypto.lua
    Adds a new test file `test/automated/crypto_spec.lua` to cover the
    `lua/spm/crypto.lua` module.
    
    The tests cover the following:
    - `generate_hash` should generate a sha256 hash for a string.
    - `generate_hash` should return an error if the content is not a string.

---
+++ test/automated/crypto_spec.lua
@@ -0,0 +1,17 @@
+---@diagnostic disable: undefined-field
+
+local crypto = require('spm.crypto')
+
+describe('crypto', function()
+  it('should generate a sha256 hash for a string', function()
+    local result = crypto.generate_hash('test')
+    assert.is_true(result:is_ok())
+    assert.are.same('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', result:unwrap())
+  end)
+
+  it('should return an error if the content is not a string', function()
+    local result = crypto.generate_hash(123)
+    assert.is_true(result:is_err())
+    assert.are.same('Content must be a string', result.error.message)
+  end)
+end)
